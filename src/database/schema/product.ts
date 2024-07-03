b30a690ff9aee612b59dd9632b2c12007d37bb27
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-07-03 16:20:01 -0300

    refactor: change schema `product` table

--- src/database/schema/product.ts
+++ src/database/schema/product.ts
@@ -1,20 +1,14 @@
-import { customType, integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { integer, numeric, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
 import { category } from './category'
 import { supplier } from './suppliers'
 import { relations } from 'drizzle-orm'
 import { inventoryMovement } from './inventory-movement'
 
-const customPrice = customType<{data: number; notNull: true}>({
-  dataType() {
-    return 'number'
-  }
-});
-
 export const product = pgTable('product', {
 	id: serial('id').primaryKey(),
 	name: text('name').notNull(),
 	description: text('description'),
-	price: customPrice('price'), //numeric('price', { precision: 7, scale: 2 }).notNull(),
+	price: numeric('price', { precision: 7, scale: 2 }).notNull(),
 	quantity: integer('quantity').notNull(),
 	idCategory: integer('idCategory').references(() => category.id, {
 		onDelete: 'cascade'
