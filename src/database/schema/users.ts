45069c53a33adc19c4fff33d75cdc0982f849db0
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-07-04 22:17:52 -0300

    refactor: add new column schema `users`

--- src/database/schema/users.ts
+++ src/database/schema/users.ts
@@ -1,18 +1,39 @@
 import { relations } from 'drizzle-orm'
-import { pgTable, serial, varchar, text, pgEnum, timestamp } from 'drizzle-orm/pg-core'
+import {
+	pgTable,
+	serial,
+	varchar,
+	text,
+	pgEnum,
+	timestamp,
+	integer,
+	foreignKey
+} from 'drizzle-orm/pg-core'
 import { inventoryMovement } from './inventory-movement'
 
 export const userRoleEnum = pgEnum('user_role', ['employee', 'admin'])
 
-export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	name: varchar('name', { length: 100 }).notNull(),
-	password: text('password').notNull(),
-	email: text('email').notNull().unique(),
-	role: userRoleEnum('role').default('employee').notNull(),
-	createdAt: timestamp('created_at').defaultNow(),
-	updatedAt: timestamp('updated_at').defaultNow()
-})
+export const user = pgTable(
+	'user',
+	{
+		id: serial('id').primaryKey(),
+		name: varchar('name', { length: 100 }).notNull(),
+		password: text('password').notNull(),
+		email: text('email').notNull().unique(),
+		role: userRoleEnum('role').default('employee').notNull(),
+		idAdmin: integer('idUser'),
+		createdAt: timestamp('created_at').defaultNow(),
+		updatedAt: timestamp('updated_at').defaultNow()
+	},
+	(table) => {
+		return {
+			parentReference: foreignKey({
+				columns: [table.idAdmin],
+				foreignColumns: [table.id]
+			})
+		}
+	}
+)
 
 export const userRelations = relations(user, ({ many }) => ({
 	inventoryMovement: many(inventoryMovement)
