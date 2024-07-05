01d6e753785e61dd5952e2d4593c50ff70e51ce9
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-07-05 14:37:11 -0300

    refactor: refatora model `auth`

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -4,23 +4,27 @@ export const AuthDTO = new Elysia().model({
 	auth: t.Object({
 		name: t.String({
 			minLength: 1,
+			maxLength: 100,
 			format: 'regex',
 			pattern: `^(?!\\s*$).+`,
-			default: 'Name user',
+			default: 'nome completo com quantidade máxima de até 100 caracteres',
 			error: 'o campo nome não pode ser vazio'
 		}),
 		password: t.String({
 			minLength: 8,
+			maxLength: 15,
 			format: 'regex',
 			pattern: `^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])\\S{8,15}$`,
-			default: 'Name user',
+			default:
+				'A senha deve ter entre 8 a 15 caracteres, Pelo menos uma letra maiúscula, Pelo menos uma letra minúscula, Pelo menos um número, Pelo menos um caractere especial (@, $, !, %, *, ?, &)',
 			error: 'A senha deve ter entre 8 a 15 caracteres, Pelo menos uma letra maiúscula, Pelo menos uma letra minúscula, Pelo menos um número, Pelo menos um caractere especial (@, $, !, %, *, ?, &)'
 		}),
 		email: t.String({
-			format: 'email'
+			format: 'email',
+			default: 'informe um email ex: test@email.com'
 		})
 	}),
 	'auth.cookie': t.Cookie({
-    value: t.Optional(t.String())
+		value: t.Optional(t.String())
 	})
 })
