27351f1ce70a765599a2c1feec83e4bbd0dbaea7
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-07-14 13:25:06 -0300

    add novo metodo `utils`

--- src/services/utils.service.ts
+++ src/services/utils.service.ts
@@ -11,4 +11,17 @@ export abstract class ServiceUtils {
 	static calculateTotalPages(totalItems: number, itemsPorPage: number) {
 		return Math.ceil(totalItems / itemsPorPage)
 	}
+
+	static omit<Data extends object, Keys extends keyof Data>(
+		data: Data,
+		keys: Keys[]
+	): Omit<Data, Keys> {
+		const result = { ...data }
+
+		for (const key of keys) {
+			delete result[key]
+		}
+
+		return result as Omit<Data, Keys>
+	}
 }
