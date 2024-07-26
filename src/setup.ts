b25964443a0308e904dfa838cb40c8e393e1a82d
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-07-26 13:27:06 -0300

    define tempo expiração `token` jwt

--- src/setup.ts
+++ src/setup.ts
@@ -5,7 +5,8 @@ export const jwtPlugin = new Elysia()
 	.use(
 		jwt({
 			name: 'jwt',
-			secret: import.meta.env.JWT_SECRET_KEY!
+			secret: import.meta.env.JWT_SECRET_KEY!,
+			exp: '1 day'
 		})
 	)
 	.decorate('jwt', jwt)
